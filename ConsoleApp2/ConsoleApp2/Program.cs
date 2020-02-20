a40cff71459bcb0923580e57239eb395eea964ef
Jamil Zakaria <mjzakaria16@gmail.com>
2020-02-20 10:43:42 +0600

    second practice added

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even number from 1 to 100");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
