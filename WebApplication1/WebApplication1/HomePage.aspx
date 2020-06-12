11cab1e9d1e7f5627fdb5d4a7665defe6527a396
Jamil Zakaria <mjzakaria16@gmail.com>
2020-06-13 05:49:02 +0600

    first web app

---
+++ WebApplication1/WebApplication1/HomePage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="WebApplication1.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="addButton" runat="server" Text="Add" OnClick="addButton_Click" />
+        <br />
+        <br />
+        <asp:Button ID="showButton" runat="server" Text="Show" OnClick="showButton_Click" />
+        <br />
+        <br />
+        <asp:ListBox ID="nameListBox" runat="server"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/HomePage.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        List<string> nameList=new List<string>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            if (ViewState["Name"] != null)
+            {
+                nameList =(List<string>) ViewState["Name"];
+            }
+            nameList.Add(name);
+            ViewState["Name"] = nameList.ToString();
+        }
+
+        protected void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
